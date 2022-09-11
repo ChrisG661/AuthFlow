7735e7bb1b1629c04e2d147b42420abd9338bc2f
ChrisG661 <59037609+ChrisG661@users.noreply.github.com>
2022-09-11 15:04:55 +0800

    Update GitHub link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -291,7 +291,7 @@
           </li>
           <li>
             <a
-              href="https://github.com/ChrisG661"
+              href="https://github.com/ChrisG661/AuthFlow"
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               aria-current="page">Lihat sumber</a
             >
