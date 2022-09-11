91814c05f4011037feedad00ad46187e4ea3c2d8
ChrisG661 <59037609+ChrisG661@users.noreply.github.com>
2022-09-11 17:17:49 +0800

    Update homepage text padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,7 +74,7 @@
   </div>
   <div class="flex items-center flex-col z-10 text-center">
     <h1
-      class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      class="px-4 mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
     >
       Sumatif Informatika Kelas 11 Kuartal I
     </h1>
