59ebaad173aa6d27c8e02f97db100d9fe2b86fc2
ChrisG661 <59037609+ChrisG661@users.noreply.github.com>
2022-09-11 17:44:11 +0800

    Update Cloudflare Pages environment

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,25 +1,28 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+/// <reference types="@sveltejs/kit" />
+/// <reference types="@sveltejs/adapter-cloudflare" />
 declare namespace App {
   // interface Locals {}
   // interface PageData {}
   // interface Platform {}
   interface Platform {
-    env: {
-      COUNTER: DurableObjectNamespace;
+    env?: {
+      //YOUR_KV_NAMESPACE: KVNamespace;
+      //YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
     };
     context: {
       waitUntil(promise: Promise<any>): void;
     };
-    caches: CacheStorage & { default: Cache }
+    caches: CacheStorage & { default: Cache };
   }
   interface UserSession {
-    user: import('@supabase/supabase-js').User
-    accessToken?: string
+    user: import("@supabase/supabase-js").User;
+    accessToken?: string;
   }
   interface Locals extends UserSession {
-    error: import('@supabase/supabase-js').ApiError
+    error: import("@supabase/supabase-js").ApiError;
   }
 
   interface Session extends UserSession { } // interface Platform {} // interface Stuff {}
